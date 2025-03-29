5a725b80168ca9d00de3d324e7872ce18aa5c498
AchuRaj <130690175+rock-star-2003@users.noreply.github.com>
2025-03-29 13:04:39 +0530

    third commit

--- DiseasePrediction.py
+++ DiseasePrediction.py
@@ -5,14 +5,49 @@ import statistics
 import pickle
 import streamlit as st
 import pandas as pd
+import statistics
+from sklearn.preprocessing import LabelEncoder
+encorder = LabelEncoder()
 
 # Load the model using pickle
 
 knn_model = joblib.load("knn_model_predict_desease.pkl")
 nb_model = joblib.load("nb_model_predict_desease.pkl")
 svm_model = joblib.load("svm_model_predict_desease.pkl")
+encoder = joblib.load("encoder.pkl")
 
+
+    
 diseases = pd.read_csv('diseases.csv')
-symptoms = pd.read_csv('symptoms.csv')
+symptoms = [j.replace('_',' ') for i in pd.read_csv('symptoms.csv').values.tolist() for j in i]
+inp_symptoms = st.multiselect("symptom_index" , symptoms)
+symptoms_index = {}
+for index,val in enumerate(symptoms):
+    symptoms = val
+    symptoms_index[val] = index
+    
+data_dict = {
+    "symptom_index" : symptoms_index,
+    'classes' : encoder.classes_
+}
+    
+def predict_disease (symptoms):
+    input_data = [0] * len(data_dict["symptom_index"])
+    
+    for symtom in symptoms:
+        index = data_dict["symptom_index"][symtom]
+        input_data[index] = 1
+    
+    input_data = np.array(input_data).reshape(1,-1)
+    
+    Knn_predict = data_dict['classes'][knn_model.predict(input_data)][0]
+    nb_predict = data_dict['classes'][ nb_model.predict(input_data)][0]
+    svm_predict = data_dict['classes'][svm_model.predict(input_data)][0]
 
-data = st.dropdown(diseases)
+    final_pred = statistics.mode([Knn_predict,nb_predict,svm_predict])
+    return final_pred
+
+
+
+if st.button('find Disease'):
+    st.text(predict_disease(inp_symptoms))
